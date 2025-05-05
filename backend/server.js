ac50625c24ae60fa907886092944d9fe3746b4d2
Eldridge Grady <158190828+Egrady5@users.noreply.github.com>
2025-05-05 16:55:28 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,15 +6,15 @@ const app = express();
 app.use(express.json());
 
 // Example route
-app.get('/', async (req, res) => {
-  try {
-    const result = await pool.query('SELECT NOW()');
-    res.send(`Database connected: ${result.rows[0].now}`);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Database connection error');
-  }
-});
+//app.get('/', async (req, res) => {
+  //try {
+    //const result = await pool.query('SELECT NOW()');
+   // res.send(`Database connected: ${result.rows[0].now}`);
+  //} catch (err) {
+   // console.error(err);
+    //res.status(500).send('Database connection error');
+  //}
+//});
 
 // Register a user (for testing)
 app.post('/register', async (req, res) => {
