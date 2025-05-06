8b0c593fc0c1de2fa97bd964233846a61086f07c
Lwalter005 <lwalter005@outlook.com>
2025-05-05 20:38:53 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const pool = require('./db');
 const cors = require('cors');
 require('dotenv').config();
-//hi
+
 const app = express();
 app.use(express.json());
 app.use(cors({
